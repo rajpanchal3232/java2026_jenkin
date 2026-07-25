5ef19b3c177f248164a8651a783b0ca49eb9ef0b
rajpanchal3232 <raj.panchal3232@gmail.com>
2026-07-25 12:07:46 +0530

    Remove print method call from main
    Removed call to print method from main.

--- javanew.java
+++ javanew.java
@@ -2,6 +2,5 @@ public class javanew {
     public static void main(String[] args) {
         System.out.println("Welcome to New Jenkins Project and Batch 44 of CloudDevOps Hub.");
         System.out.println("Welcome to New Jenkins Project and Batch 44 of CloudDevOps Hub.");
-        print();
     }
 }
