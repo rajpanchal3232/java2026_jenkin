09d6845fef9c6bd13d6b6f423e592b7802beea7c
rajpanchal3232 <raj.panchal3232@gmail.com>
2026-07-25 12:06:35 +0530

    Add print method call in main method

--- javanew.java
+++ javanew.java
@@ -2,5 +2,6 @@ public class javanew {
     public static void main(String[] args) {
         System.out.println("Welcome to New Jenkins Project and Batch 44 of CloudDevOps Hub.");
         System.out.println("Welcome to New Jenkins Project and Batch 44 of CloudDevOps Hub.");
+        print();
     }
 }
